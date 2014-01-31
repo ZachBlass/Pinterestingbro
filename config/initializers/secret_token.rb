ffa26e42503d7fcec97c8279f1e713b6172546b3
Zach Blass <zachary.blass@gmail.com>
2014-01-31 18:05:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinterestingbro::Application.config.secret_key_base = '3620804447ec9b5b7c871b4b1695593b15b129178609b2e31c8cf72ff29fc9b7c87560fe8f8735d32b3bcea50bf2ca5ffca70c095426a29a8ec54c210f8ff46e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pinterestingbro::Application.config.session_store :cookie_store, key: '_pinterestingbro_session'
